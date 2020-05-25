bfd7249c7de88887cfef8041d12aa5ed6f8ac620
Sonya Gould <sonyavgould@gmail.com>
2020-05-25 22:51:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,15 +35,10 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if (index.between?(0, 8) == true && position_taken?(board, index) == false)
-       true
-     else
-       false
-  end
+  index.between?(0, 8) && !(position_taken?(board, index))
 end
   
 def turn(board)
-  input = gets
   index = 0
   until valid_move?(board, index)
     puts "Please enter 1-9:"
